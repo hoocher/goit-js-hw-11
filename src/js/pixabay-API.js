a68a47fd6336fdbdd98a72f6e814efaf067c31c9
Dmytro Huriev <hooch.inbox@gmail.com>
2023-08-10 22:29:38 +0300

    rewrite Pixabay-api js to class

--- src/js/pixabay-API.js
+++ src/js/pixabay-API.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const formEl = document.querySelector('.search-form');
+export class PIXABAY_API {
+  per_page = 40;
+  API_KEY = '38646784-30fa50ee56e34cceca0d99061';
 
-export async function getImage(tag, pag) {
-  let find = `${tag}`;
-  let page = `${pag}`;
-  let per_page = 40;
-  const API_KEY = '38646784-30fa50ee56e34cceca0d99061';
-  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${find}&per_page=${per_page}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true`;
+  async getImage(tag, pag) {
+    let url = `https://pixabay.com/api/?key=${this.API_KEY}&q=${tag}&per_page=${
+      this.per_page
+    }&page=${pag || 1}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-  return await axios.get(`${URL}`);
+    return await axios.get(url);
+  }
 }
